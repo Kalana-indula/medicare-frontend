547ea965f92893bd773a0a07008fc647cb4f4d7b
Kalana-indula <kalanasmedia@outlook.com>
2025-04-28 12:50:09 +0530

    add admin completed ✔️

--- src/pages/AddAdmin.jsx
+++ src/pages/AddAdmin.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Input from "../styles/Input";
+import axios from "axios";
 
 const AddAdmin = () => {
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleFirstName = (event) => {
         // console.log(event);
@@ -25,8 +26,43 @@ const AddAdmin = () => {
         setPassword(event.target.value);
     }
 
-    const handleSubmit=()=>{
-        
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        // creating the  js object to fetch data
+        const data = {
+            "firstName": firstName,
+            "lastName": lastName,
+            "email": email,
+            "password": password
+        };
+
+        try {
+            const response = await axios.post(`http://localhost:8081/api/admins`, data, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            console.log(response.data);
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setPassword("");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+
+        console.log("cancel button clicked");
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
     }
 
     return (
@@ -37,15 +73,39 @@ const AddAdmin = () => {
                 <form onSubmit={handleSubmit}>
                     <div className="flex flex-col ml-[20px] mt-[10px] mb-[10px] w-[400px]">
                         {/* styles are created using an external component */}
-                        <Input type="text" placeholder="First Name" onChange={handleFirstName} />
-                        <Input type="text" placeholder="Last Name" onChange={handleLastName} />
-                        <Input type="text" placeholder="Email" onChange={handleEmail} />
-                        <Input type="text" placeholder="Password" onChange={handlePassword} />
+                        <Input type="text"
+                            placeholder="First Name"
+                            onChange={handleFirstName}
+                            value={firstName}
+                        />
+                        <Input type="text"
+                            placeholder="Last Name"
+                            onChange={handleLastName}
+                            value={lastName}
+                        />
+                        <Input type="text"
+                            placeholder="Email"
+                            onChange={handleEmail}
+                            value={email}
+                        />
+                        <Input type="password"
+                            placeholder="Password"
+                            onChange={handlePassword}
+                            value={password}
+                        />
                         <div>
-                            <button className="bg-blue-600 py-[10px] w-[100%] my-[5px] cursor-pointer hover:bg-blue-500 active:bg-blue-900">Save</button>
+                            <button className="bg-blue-600 py-[10px] w-[100%] my-[5px] cursor-pointer hover:bg-blue-500 active:bg-blue-900"
+                                type="submit"
+                            >
+                                Save
+                            </button>
                         </div>
                         <div>
-                            <button className="bg-red-600 py-[10px] w-[100%] my-[5px] cursor-pointer hover:bg-red-500 active:bg-red-700">Cancel</button>
+                            <button className="bg-red-600 py-[10px] w-[100%] my-[5px] cursor-pointer hover:bg-red-500 active:bg-red-700"
+                                onClick={handleCancel} type="button"
+                            >
+                                Cancel
+                            </button>
                         </div>
                     </div>
                 </form>
--- src/styles/Input.jsx
+++ src/styles/Input.jsx
@@ -1,11 +1,12 @@
 //applying styles for all reuse and encapsulate styling
-const Input = ({ placeholder, type = "text",onChange }) => {
+const Input = ({ placeholder, type = "text",onChange,value }) => {
     return (
         <input
             type={type}
             placeholder={placeholder}
             className="border-gray-800 border-[2px] mt-[10px] mb-[10px] p-[10px]"
             onChange={onChange}
+            value={value}
         />
     );
 }
